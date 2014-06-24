45067b60cd87325fcc32e0031549faccb8b1bb07
theptrk <patrick.tran06@gmail.com>
2014-06-24 14:00:09 -0700

    (docs) Added docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 //! Body Parser middleware for Iron
+//! 
+//! This middleware focuses on parsing incoming data from client requests. Body Parser 
+//! has a function `new`, that creates an instance of the middleware and if both invoked 
+//! and linked to `server.chain`, it will attach a Parsed<Json> struct to the `Alloy`.
 #![crate_id = "bodyparser"]
 #![license = "MIT"]
 
@@ -11,13 +15,19 @@ use iron::middleware::{Status, Continue, Unwind};
 use serialize::json;
 use serialize::json::Json;
 
-/// The Parsed type holds a Json object that Body Parser will populate.
+/// The Parsed type holds the Json object that is parsed from incoming data.
+/// This struct is created with a call to `BodyParser.new` to store on an alloy. 
 #[deriving(Clone)]
 pub struct Parsed(pub Json);
 
+/// `Middleware` for parsing passed in data and storing the data as JSON.
+/// Using `new` and adding this link to the chain is the current implementation.
 #[deriving(Clone)]
 pub struct BodyParser;
 
+/// Using the `new` function will activate this middleware and parse incoming data
+/// and insert it only an `Alloy`. Subsequent middlware will now have access by using
+/// the `find` function from `Alloy`.
 impl BodyParser {
     pub fn new() -> BodyParser {
         BodyParser
