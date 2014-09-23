c2af3e3f625aa30b0d6545a47dc7a58bea06076d
Jonathan Reem <jonathan.reem@gmail.com>
2014-09-23 06:22:47 -0700

    (refactor) Become a Plugin.

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,38 @@
 #![doc(html_logo_url = "https://avatars0.githubusercontent.com/u/7853871?s=128", html_favicon_url = "https://avatars0.githubusercontent.com/u/7853871?s=256", html_root_url = "http://ironframework.io/core/bodyparser")]
+#![feature(default_type_params)]
+#![license = "MIT"]
+
 //! Body Parser middleware for Iron
 //!
 //! This middleware parses incoming JSON data from client requests. On an empy
 //! request, or on malformed data, the chain is not unwound, but rather
 //! nothing is inserted into the `Alloy`. Middleware further down the chain
 //! must take care to handle this robustly.
-#![license = "MIT"]
 
 extern crate iron;
 extern crate serialize;
+extern crate plugin;
 
-use iron::{Request, Response, Middleware, Status, Continue};
+use iron::Request;
+use iron::typemap::Assoc;
+
+use plugin::{PluginFor, Phantom};
 
 use serialize::json;
 use serialize::json::Json;
 
-/// The Parsed type holds the Json object that was parsed from incoming data.
-#[deriving(Clone)]
-pub struct Parsed(pub Json);
-
 #[deriving(Clone)]
 pub struct BodyParser;
 
-impl BodyParser {
-    /// Create a new `BodyParse`, for linking into a `Chain`
-    pub fn new() -> BodyParser {
-        BodyParser
+impl Assoc<Json> for BodyParser {}
+
+impl PluginFor<Request, Json> for BodyParser {
+    fn eval(req: &Request, _: Phantom<BodyParser>) -> Option<Json> {
+        if !req.body.is_empty() {
+            json::from_str(req.body.as_slice()).ok()
+        } else {
+            None
+        }
     }
 }
 
-impl Middleware for BodyParser {
-    fn enter(&mut self, req: &mut Request, _ : &mut Response) -> Status {
-        if !req.body.is_empty() {
-            match json::from_str(req.body.as_slice()) {
-                Ok(parsed) => {
-                    req.extensions.insert::<Parsed>(Parsed(parsed));
-                },
-                Err(_) => {
-                    return Continue;
-                }
-            }
-        }
-        Continue
-    }
-}
