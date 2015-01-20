a21e353002034ee456b81224b91ee71d68526408
John Barker <jebarker@gmail.com>
2015-01-19 19:55:54 -0500

    Update to build with nightly rust
     * Add plugin/typemap crates
     * Assoc<T> becomes Key
     * PluginFor<, T> becomes PluginFor<>
     * .ok() from_str
     * New response format in example
     * Removed unneeded feature directives globs and default_type_params

--- examples/bodyparser.rs
+++ examples/bodyparser.rs
@@ -1,14 +1,12 @@
-#![feature(globs)]
 extern crate iron;
 extern crate bodyparser;
 extern crate serialize;
 
 use iron::prelude::*;
 use iron::status;
-use iron::response::modifiers::{Body, Status};
 use bodyparser::BodyParser;
 
-#[deriving(Decodable, Clone, Show)]
+#[derive(Decodable, Clone, Show)]
 struct JsonParams {
     name: String,
     age: Option<i8>,
@@ -17,10 +15,10 @@ struct JsonParams {
 fn log_json(req: &mut Request) -> IronResult<Response> {
     let parsed = req.get::<BodyParser<JsonParams>>();
     match parsed {
-        Some(params) => println!("Parsed json:\n{}", params),
+        Some(params) => println!("Parsed json:\n{:?}", params),
         None => println!("Invalid or no json!"),
     }
-    Ok(Response::new().set(Status(status::Ok)))
+    Ok(Response::with(status::Ok))
 }
 
 // `curl -i "localhost:3000/" -H "application/json" -d '{"name":"jason","age":"2"}'`
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,33 @@
 #![crate_name = "bodyparser"]
-#![feature(default_type_params)]
 
 //! Body Parser Plugin for Iron
 //!
 //! This plugin parses JSON out of an incoming Request.
 
 extern crate iron;
-extern crate serialize;
 extern crate plugin;
+extern crate serialize;
+extern crate typemap;
 
 use iron::Request;
-use iron::typemap::Assoc;
+use typemap::Key;
 
-use plugin::{PluginFor, Phantom};
+use plugin::{Plugin, Phantom};
 
 use serialize::{json, Decodable};
-use serialize::json::{Decoder, DecoderError};
 use std::str;
 
 #[deriving(Clone)]
-pub struct BodyParser<T: Decodable<Decoder, DecoderError>>;
+pub struct BodyParser<T: Decodable>;
 
-impl<T: 'static + Decodable<Decoder, DecoderError>> Assoc<T> for BodyParser<T> {}
+impl<T: 'static + Decodable> Key for BodyParser<T> {
+    type Value = T;
+}
 
-impl<T: Decodable<Decoder, DecoderError>> PluginFor<Request, T> for BodyParser<T> {
+impl<T: 'static + Decodable> Plugin<Request> for BodyParser<T> {
     fn eval(req: &mut Request, _: Phantom<BodyParser<T>>) -> Option<T> {
         if !req.body.is_empty() {
-            str::from_utf8(req.body.as_slice())
+            str::from_utf8(req.body.as_slice()).ok()
                 .and_then(|body| json::from_str(body).ok())
                 .and_then(|json| Decodable::decode(&mut json::Decoder::new(json)).ok())
         } else {
