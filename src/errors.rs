252b3f6381b70a9738a5e2376c1782f06716e0e6
Jimmy Cuadra <jimmy@jimmycuadra.com>
2015-12-18 00:41:33 -0800

    Use serde for JSON and struct deserialization.

--- src/errors.rs
+++ src/errors.rs
@@ -2,14 +2,14 @@ use std::error::Error as StdError;
 use std::fmt;
 use std::io;
 use std::str;
-use rustc_serialize::json;
+
+use serde_json;
 
 #[derive(Debug)]
 pub enum BodyErrorCause {
     Utf8Error(str::Utf8Error),
     IoError(io::Error),
-    ParserError(json::ParserError),
-    DecoderError(json::DecoderError)
+    JsonError(serde_json::Error),
 }
 
 #[derive(Debug)]
